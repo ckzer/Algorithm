ce19482a996da0ec62197a985b961cc675900b9d
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-04-14 00:27:53 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+n = int(input())
+arr = list(map(int, input().split()))
+dq = deque([(idx+1, val) for idx, val in enumerate(arr)])
+res = []
+while dq:
+    idx, move = dq.popleft()
+    res.append(idx)
+    if not dq:
+        break
+    if move > 0:
+        dq.rotate(-(move-1))
+    else:
+        dq.rotate(-move)
+
+print(*res)
