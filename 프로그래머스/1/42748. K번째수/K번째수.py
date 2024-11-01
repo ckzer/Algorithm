e03920dabcde13deb0f70d79583a265d66613bf2
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-11-02 08:25:29 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        li=[]
+        li=array[commands[i][0]-1:commands[i][1]]
+        li.sort()
+        answer.append(li[commands[i][2]-1])
+    return answer
